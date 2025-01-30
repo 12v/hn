944e4cf04b1ff581446df42d0dd509c50fbac60b
SeveralLargeRocks <rossoliver32@gmail.com>
2025-01-30 19:31:09 +0000

    Small edits

--- train_embeddings.py
+++ train_embeddings.py
@@ -47,8 +47,8 @@ window_size = 1
 batch_size = 512
 embedding_dim = 64
 epochs = 3
-arch = "cbow"
-# arch = "skipgram"
+# arch = "cbow"
+arch = "skipgram"
 initial_lr = 0.001
 
 
@@ -58,67 +58,73 @@ vocab_size = len(tokeniser.vocab_mapping)
 
 torch.manual_seed(1989)
 
-with open(os.path.join(script_dir, "sources/normalised_corpus_1.txt"), "r") as f:
-    corpus = f.read()
 
-dataset = generate_pairs_from_tokens(tokeniser, corpus.split("\n"), window_size)
-dataloader = torch.utils.data.DataLoader(dataset, batch_size, shuffle=True)
+def train_embeddings(corpus_path):
+    with open(os.path.join(script_dir, corpus_path), "r") as f:
+        corpus = f.read()
 
-if torch.cuda.is_available():
-    device = torch.device("cuda")
-elif torch.backends.mps.is_available():
-    device = torch.device("mps")
-else:
-    device = torch.device("cpu")
+    dataset = generate_pairs_from_tokens(tokeniser, corpus.split("\n"), window_size)
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size, shuffle=True)
+
+    if torch.cuda.is_available():
+        device = torch.device("cuda")
+    elif torch.backends.mps.is_available():
+        device = torch.device("mps")
+    else:
+        device = torch.device("cpu")
+
+    wandb.init(
+        project="mlx6-word2vec",
+        config={
+            "learning_rate": initial_lr,
+            "architecture": arch,
+            "dataset": corpus_path,
+            "epochs": epochs,
+        },
+    )
+
+    model = Word2Vec(arch, vocab_size, embedding_dim)
+
+    print(arch)
+    print("param count:", sum(p.numel() for p in model.parameters()))
+
+    model.to(device)
+
+    optimiser = torch.optim.Adam(model.parameters(), lr=initial_lr)
+
+    for epoch in range(epochs):
+        print(f"Epoch {epoch + 1} started")
+        prgs = tqdm.tqdm(dataloader, desc=f"Epoch {epoch+1}", leave=False)
+
+        for center, context in prgs:
+            center, context = center.to(device), context.to(device)
+
+            cbow_rand = torch.randint(0, vocab_size, (center.size(0),)).to(device)
+            skipgram_rand = torch.randint(0, vocab_size, (context.size(0), 2)).to(
+                device
+            )
+
+            if arch == "cbow":
+                rand = cbow_rand
+            else:
+                rand = skipgram_rand
+
+            optimiser.zero_grad()
+            loss = model(center, context, rand)
+
+            loss.backward()
+            optimiser.step()
+
+            wandb.log({"loss": loss.item()})
+
+    torch.save(model.state_dict(), os.path.join(script_dir, "weights.pt"))
+    print("Uploading...")
+    artifact = wandb.Artifact("model-weights", type="model")
+    artifact.add_file("./weights.pt")
+    wandb.log_artifact(artifact)
+    print("Done!")
+    wandb.finish()
 
 
-wandb.init(
-    project="mlx6-word2vec",
-    config={
-        "learning_rate": initial_lr,
-        "architecture": arch,
-        "dataset": "text8",
-        "epochs": epochs,
-    },
-)
-
-model = Word2Vec(arch, vocab_size, embedding_dim)
-
-print(arch)
-print("param count:", sum(p.numel() for p in model.parameters()))
-
-model.to(device)
-
-optimiser = torch.optim.Adam(model.parameters(), lr=initial_lr)
-
-for epoch in range(epochs):
-    print(f"Epoch {epoch + 1} started")
-    prgs = tqdm.tqdm(dataloader, desc=f"Epoch {epoch+1}", leave=False)
-
-    for center, context in prgs:
-        center, context = center.to(device), context.to(device)
-
-        cbow_rand = torch.randint(0, vocab_size, (center.size(0),)).to(device)
-        skipgram_rand = torch.randint(0, vocab_size, (context.size(0), 2)).to(device)
-
-        if arch == "cbow":
-            rand = cbow_rand
-        else:
-            rand = skipgram_rand
-
-        optimiser.zero_grad()
-        loss = model(center, context, rand)
-
-        loss.backward()
-        optimiser.step()
-
-        wandb.log({"loss": loss.item()})
-
-
-torch.save(model.state_dict(), os.path.join(script_dir, "weights.pt"))
-print("Uploading...")
-artifact = wandb.Artifact("model-weights", type="model")
-artifact.add_file("./weights.pt")
-wandb.log_artifact(artifact)
-print("Done!")
-wandb.finish()
+train_embeddings("sources/normalised_corpus_1.txt")
+train_embeddings("sources/normalised_corpus_2.txt")
